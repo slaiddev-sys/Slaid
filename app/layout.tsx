352e1ec63d0809392d9238223019aecae6708414
Manuel Leal Betancor <manuellealbetancor@MacBook-Pro-de-Manuel.local>
2025-12-01 13:11:17 +0100

    Add Google Analytics tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import "./fonts.css";
 import { AuthProvider } from "../components/AuthProvider";
@@ -89,6 +90,20 @@ export default function RootLayout({
         className="antialiased"
         suppressHydrationWarning={true}
       >
+        {/* Google Tag Manager (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-H3MH6EM5TB"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-H3MH6EM5TB');
+          `}
+        </Script>
         <AuthProvider>
           {children}
         </AuthProvider>
